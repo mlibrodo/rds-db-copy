63e8d7933fedc79135eddceb8c768053b42a2680
Michael Librodo <librodo@tatari.tv>
2022-04-27 10:09:52 -0700

    add pg_isready command

---
+++ postgres/pgcommands/pgready.go
@@ -0,0 +1,33 @@
+package pgcommands
+
+var (
+	// PGDumpCmd is the path to the `pg_dump` executable
+	PGIsReadyCmd = "pg_isready"
+)
+
+type IsReady struct {
+	*Conn
+	// Seconds to wait when attempting connection. 0 disables (default: 3)
+	Timeout int
+}
+
+func (x *IsReady) Exec() Result {
+	execFn := GenericExec(PGIsReadyCmd, x.Conn, x.ParseArgs)
+
+	return execFn()
+
+}
+
+func (x *IsReady) ParseArgs() []string {
+	var args []string
+
+	if y := x.DBHostAsCmdArg(); y != nil {
+		args = append(args, *y)
+	}
+
+	if y := x.DBPortAsCmdArg(); y != nil {
+		args = append(args, *y)
+	}
+
+	return args
+}
